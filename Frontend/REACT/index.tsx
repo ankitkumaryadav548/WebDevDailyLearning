223e5ce277fd7ad7691e8e96e977481ea1b7b52c
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-05-26 12:15:57 +0530

    REACT

---
+++ Frontend/REACT/index.tsx
@@ -0,0 +1,30 @@
+//React is a JavaScript library used to build user interfaces (UI)
+//React was created by Meta (Facebook)
+// It helps developers create:
+// websites,
+// dashboards,
+// mobile apps,
+// admin panels,
+// chat apps,
+// ecommerce sites 
+
+// Why React is Popular
+// Without React => 
+// updating UI manually becomes difficult,
+// code becomes messy,
+// large apps are hard to manage
+
+// React solves this using => 
+// Components
+// State
+// Reusable UI
+
+
+
+function User() {
+  return (
+    <h1>Hello User</h1>
+  )
+}
+
+export default User
