72f09a4882f19d837997fbbd51e02ac44ba19571
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-05-27 23:50:42 +0530

    prop in react.js

---
+++ Frontend/REACT/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Rahul" age={22} />
+    </div>
+  );
+}
+
+export default App;
