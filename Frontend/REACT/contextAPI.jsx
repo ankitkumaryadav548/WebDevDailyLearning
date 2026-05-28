aa1975a45461a772c733ec249186640d8afb8925
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-05-28 23:43:26 +0530

    prop drilling

--- Frontend/REACT/contextAPI.jsx
+++ Frontend/REACT/contextAPI.jsx
@@ -1,3 +1,24 @@
+//props drilling => prop drilling happens when data is passed from a parent component down through multiple nested components using props, even when some intermediate components don’t actually use the data.
+// Prop Drilling Can Be a Problem because =>
+// magine you have:
+
+// many nested components
+// many props
+// large applications
+
+// Then:
+
+// components become cluttered
+// changing prop names becomes harder
+// maintenance gets difficult
+
+// Example problem:
+// App → Layout → Sidebar → Menu → Profile → Avatar
+// If only Avatar needs the data, every component in between must still pass it.
+
+// To avoid prop drilling => we will use context API
+//Context API lets components access shared data directly without passing props through every level.
+
 const UserContext = React.createContext();
 
 function App() {
