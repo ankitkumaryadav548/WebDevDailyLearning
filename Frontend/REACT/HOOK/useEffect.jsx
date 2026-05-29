49a5bf3039dd791f1dda3a28e711a6003d4ec55f
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-05-29 23:49:49 +0530

    API fetch example

--- Frontend/REACT/HOOK/useEffect.jsx
+++ Frontend/REACT/HOOK/useEffect.jsx
@@ -21,3 +21,33 @@ function App() {
 }
 
 export default App;
+
+
+
+//API fetch Example
+import React, { useEffect, useState } from "react";
+
+function Users() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUsers(data));
+
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+
+      {users.map(user => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
