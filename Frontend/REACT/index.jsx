8057dcecf1ab3e35abd99833ed3efab7d1ac14fb
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-03 23:57:27 +0530

    update

--- Frontend/REACT/index.jsx
+++ Frontend/REACT/index.jsx
@@ -27,3 +27,12 @@ function User() {
 }
 
 export default User
+
+
+import { Component } from 'react';
+class Greeting extends Component {
+  render() {
+    const { name } = this.props;
+    return <h1>Hello, {name}!</h1>;
+  }
+}
