9dfe02c63edd4625a6f0d8cbb5886659ea26ac95
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-08-11 16:55:54 +0530

    NEXTJS

---
+++ Backend/NEXTJS/my-app/app/about/page.js
@@ -0,0 +1,8 @@
+export default function about(){
+    return(<div>
+        <h1>
+            Welcome to About Page
+        </h1>
+    </div>
+    )
+}
---
+++ Backend/NEXTJS/my-app/app/page.js
@@ -0,0 +1,8 @@
+export default function home(){
+  return(
+    <div>
+      <h1>Hello I am in bteck cse 3rd year</h1>
+    </div>
+  )
+}
+
