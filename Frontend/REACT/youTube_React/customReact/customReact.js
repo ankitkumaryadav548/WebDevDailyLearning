2ca48b6c84c6026c9dcaf677f71b71e28df1a91e
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-08-12 23:49:48 +0530

    custom react another method

--- Frontend/REACT/youTube_React/customReact/customReact.js
+++ Frontend/REACT/youTube_React/customReact/customReact.js
@@ -1,1 +1,34 @@
+//Method 1
+// function customRender(reactElement , container){
+//     const domElement = document.createElement(reactElement.type)
+//     domElement.innerHTML = reactElement.Children
+//     domElement.setAttribute('href',reactElement.props.href)
+//     domElement.setAttribute('target',reactElement.props.target)
+
+//     container.appendChild(domElement)
+// }
+
+//Method 2
+function customRender(reactElement , container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop === 'children') continue ;
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+
+const reactElement = {
+    type : 'a',
+    props:{
+        href:'https://google.com',
+        target : '_blank'
+    },
+    Children : 'click me to visit to google'
+}
+
 const mainContainer =  document.querySelector('#root')
+
+customRender(reactElement,mainContainer)
