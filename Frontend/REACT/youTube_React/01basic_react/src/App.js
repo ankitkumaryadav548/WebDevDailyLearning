ab2adca26e09059c1f5a9c8e0cf2b85ebf250bf5
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-08-19 11:32:25 +0530

    understanding basic folder structure

--- Frontend/REACT/youTube_React/01basic_react/src/App.js
+++ Frontend/REACT/youTube_React/01basic_react/src/App.js
@@ -5,10 +5,26 @@ function App() {
   return (
     <>
     <h1>chai is ready</h1>
-    <Chai/>
+    <Chai/>  
+    {/* "Render the Chai component here." */}
     </>
   
   );
 }
 
 export default App;
+
+// Babel in React.js is a tool that converts modern JavaScript and JSX into JavaScript that browsers can understand.
+
+// In a React project => 
+// The basic flow is:
+
+// You write in 
+//    ↓
+// JSX + modern JavaScript
+//    ↓
+// Babel convert into
+//    ↓
+// Regular JavaScript to make understand by 
+//    ↓
+// Browser
