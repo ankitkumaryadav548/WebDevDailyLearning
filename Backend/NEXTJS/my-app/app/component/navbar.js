179c0c8065abb2fef032b94dd31e79a7bb758cbf
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-09-08 23:59:47 +0530

    update

--- Backend/NEXTJS/my-app/app/component/navbar.js
+++ Backend/NEXTJS/my-app/app/component/navbar.js
@@ -1,17 +1,35 @@
+// import Link from "next/link";
 
+// export default function Navbar() {
+//   return (
+//     <>
+//       <nav className="border-green-500">
+//         <Link className="text-red-50 p-1 font-bold border-2" href="/">
+//           Home
+//         </Link>
+//         <Link className="text-blue-50 p-1 font-bold border-2" href="about">
+//           about
+//         </Link>
+//         <Link className="text-yellow-50 p-1 font-bold border-2" href="service">
+//           service
+//         </Link>
+//         <Link className="text-pink-50 p-1 font-bold border-2" href="student">
+//           student
+//         </Link>
+       
+//       </nav>
+//     </>
+//   );
+// }
 import Link from "next/link"
-
-export default function Navbar() {
-    return (
+export default function Navbar(){
+    return(
         <>
-            <nav className="border-green-500">
-                <Link className="text-red-50 p-1 font-bold border-2" href="/">Home</Link>
-                <Link className="text-blue-50 p-1 font-bold border-2" href="about">about</Link>
-                <Link className="text-yellow-50 p-1 font-bold border-2" href="service">service</Link>
-                <Link className="text-pink-50 p-1 font-bold border-2" href="student">student</Link>
-
-                
-            </nav>
+        <nav>
+            <Link href="Student">Student</Link>
+            <Link href="Student">About</Link>
+            <Link href="Student">Section</Link>
+        </nav>
         </>
     )
 }
--- Backend/NEXTJS/my-app/app/layout.js
+++ Backend/NEXTJS/my-app/app/layout.js
@@ -1,32 +1,46 @@
-// import "./globals.css";
+// // import "./globals.css";
+// import Footer from "./component/Footer";
+// import Navbar from "./component/navbar";
+// import Button from "./component/Button";
+
+// export default function RootLayout({children}) {
+// //RootLayout is function name,we can change it but for clear and conventional name we write
+// //  here children basically means:
+// // "Put the current page's content here."
+//     // console.log(props.children)
+//   // console.log(children)
+//   // console.log(children)
+//   return (    //Your function needs to return JSX.
+//               //This JSX describes the HTML structure that Next.js should render.
+//     <html lang="en">
+//      {/* //lang="en" tells the browser and accessibility tools:
+//                     //"The content of this website is primarily English." */}
+    
+//       <body className="min-h-full flex flex-col">
+//         {/* <Navbar> </Navbar> //same   */}
+//         <Navbar/>  
+//         {/* React sees this as: "Render the component called Navbar." */}
+//         {children}
+//         {/* children is the current page content. it very*/}
+//         <Button/>
+//         <Footer/>
+//         {/* React sees this as: "Render the component called Footer." */}
+
+//       </body>
+//     </html>
+//   );
+// }
+
 import Footer from "./component/Footer";
 import Navbar from "./component/navbar";
-import Button from "./component/Button";
+import Searchbox from "./component/searchbox";
 
-export default function RootLayout({children}) {
-//RootLayout is function name,we can change it but for clear and conventional name we write
-//  here children basically means:
-// "Put the current page's content here."
-    // console.log(props.children)
-  // console.log(children)
-  // console.log(children)
-  return (    //Your function needs to return JSX.
-              //This JSX describes the HTML structure that Next.js should render.
-    <html lang="en">
-     {/* //lang="en" tells the browser and accessibility tools:
-                    //"The content of this website is primarily English." */}
-    
-      <body className="min-h-full flex flex-col">
-        {/* <Navbar> </Navbar> //same   */}
-        <Navbar/>  
-        {/* React sees this as: "Render the component called Navbar." */}
-        {children}
-        {/* children is the current page content. it very*/}
-        <Button/>
-        <Footer/>
-        {/* React sees this as: "Render the component called Footer." */}
-
-      </body>
-    </html>
-  );
+export default function RootLayout({children}){
+  return(
+    <>
+    <Navbar/>
+    {children}
+    <Footer/>
+    </>
+  )
 }
